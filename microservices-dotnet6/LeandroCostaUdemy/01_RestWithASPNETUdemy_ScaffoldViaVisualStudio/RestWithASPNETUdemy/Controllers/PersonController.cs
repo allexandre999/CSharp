0f28118691a8112594886901c4d7be18364de5d4
Joao Alexandre <alexandre_efeta@hotmail.com>
2023-05-08 10:59:49 -0300

    Criacao de PersonController e Pasta Model com  classe Person

---
+++ microservices-dotnet6/LeandroCostaUdemy/01_RestWithASPNETUdemy_ScaffoldViaVisualStudio/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("soma/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSoma(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Entrada Invalida");
+        }  
+
+    }
+}
---
+++ microservices-dotnet6/LeandroCostaUdemy/01_RestWithASPNETUdemy_ScaffoldViaVisualStudio/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string Gender { get; set; }
+    }
+}
